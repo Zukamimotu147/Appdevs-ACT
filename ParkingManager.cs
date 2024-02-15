355ceaec91466fbcdd196f025f82f5bb61d35ecf
Zuka <112555506+Zukamimotu147@users.noreply.github.com>
2024-02-15 09:03:28 +0800

    Add files via upload
    fuck leo

---
+++ ParkingManager.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class ParkingManager
+    {
+        private string plateNumber;
+        private string vehicleType;
+        private string brand;
+        private int flagDown;
+        private DateTime parkIn;
+
+        public ParkingManager(string plateNumber, string brand, string vehicleType)
+        {
+            this.plateNumber = plateNumber;
+            this.brand = brand;
+            this.vehicleType = vehicleType;
+            parkIn = DateTime.Now;
+        }
+        
+
+        public string PlateNumber
+        {
+            get { return plateNumber; }
+            set { plateNumber = value; }
+        }
+
+        public string VehicleType
+        {
+            get { return vehicleType; }
+            set { vehicleType = value; }
+        }
+
+        public string Brand
+        {
+            get { return brand; }
+            set { brand = value; }
+        }
+
+        public int FlagDown
+        {
+            get { return flagDown; }
+            set { flagDown = value; }
+        }
+
+        public void printDetails()
+        {
+            Console.WriteLine("Plater Number: " + PlateNumber);
+            Console.WriteLine("Vehicle Type: " + VehicleType);
+            Console.WriteLine("Brand: " + Brand);
+            Console.WriteLine("Park in: " + parkIn);
+        }
+
+        public DateTime ParkIn
+        {
+            get { return parkIn; }
+            set { parkIn = value; }
+        }
+
+
+        public void calculateParkOut(DateTime parkOut)
+        {
+            
+            TimeSpan diff1 = parkOut.Subtract(this.ParkIn);
+
+            Console.Write(diff1);
+        }
+           
+
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] vehicleList = { "SUV", "Motorbike", "Van", "Sedan" };
+
+
+            Console.Write("Enter plater number: ");
+            string plateNumber = Console.ReadLine();
+
+            for (int i = 0; i < vehicleList.Length; ++i)
+            {
+                Console.WriteLine("{0}) {1}", i + 1, vehicleList[i]);
+            }
+
+            Console.Write("Choose vehicle type (1-4):");
+            int vehicleType = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter vehicle brand: ");
+            string brand = Console.ReadLine();
+
+            ParkingManager car = new ParkingManager(plateNumber, brand, vehicleList[vehicleType - 1]);
+
+            switch (vehicleList[vehicleType - 1])
+            {
+                case "Motorbike":
+                    car.FlagDown = 20;
+                    break;
+
+                case "SUV":
+                case "Van":
+                    car.FlagDown = 40;
+                    break;
+
+                case "Sedan":
+                    car.FlagDown = 30;
+                    break;
+
+            }
+
+          
+            car.printDetails();
+
+            Console.Write("Parkout time: ");
+            DateTime parkout = Convert.ToDateTime(Console.ReadLine());
+
+            car.calculateParkOut(parkout);
+            Console.ReadKey();
+
+            // WriteLine("{0}\n{1}\n{2}\n", plateNumber, vehicleType, brand);
+
+        }
+    }
+}
